b02ffbab7a339a5323f53c5c830a4e8de2667df7
unknown <urrizarogimer@gmail.com>
2022-01-20 16:46:30 +0800

    Separate component

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import List from '../components/Lists.js';
 class App extends React.Component {
 
+    // constructor() {
+    //     super();
+    //     this.state = {
+    //       name: "React",
+    //     };
+    // }
     render() {
         return(
         <div className="app">
             <header className="app-header">
-                <h1>My Amazing Todo-List App</h1>
+                <h1>My Amazing Todo-List App </h1>
                 <p> The most simple and amazing todo-list app</p>
-                <ul>
-                    <li className="tasks-item">Feed Birds</li>
-                    <li className="tasks-item">Feed Fish</li>
-                    <li className="tasks-item">Clean workstation</li>
-                </ul>
+                <List/>
             </header>
         </div>
         );
---
+++ src/components/Lists.js
@@ -0,0 +1,15 @@
+import React from 'react';
+class List extends React.Component {
+
+    render() {
+        return(
+                <ul>
+                    <li className="tasks-item">Feed Birds</li>
+                    <li className="tasks-item">Feed Fish</li>
+                    <li className="tasks-item">Clean workstation</li>
+                </ul>
+        );
+    };
+};
+
+export default List;
