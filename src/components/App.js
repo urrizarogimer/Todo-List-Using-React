19e31ad40ee080191663c3b2b1cd51d39e5c3426
unknown <urrizarogimer@gmail.com>
2022-01-20 16:30:40 +0800

    Add some styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 class App extends React.Component {
+
     render() {
         return(
         <div className="app">
             <header className="app-header">
                 <h1>My Amazing Todo-List App</h1>
                 <p> The most simple and amazing todo-list app</p>
+                <ul>
+                    <li className="tasks-item">Feed Birds</li>
+                    <li className="tasks-item">Feed Fish</li>
+                    <li className="tasks-item">Clean workstation</li>
+                </ul>
             </header>
         </div>
         );
