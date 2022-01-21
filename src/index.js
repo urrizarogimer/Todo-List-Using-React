353c8840032c81da6e7b60d32f5be61bb6e62524
unknown <urrizarogimer@gmail.com>
2022-01-21 13:55:40 +0800

    change to function-based

--- src/index.js
+++ src/index.js
@@ -1,14 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.js';
 import './assets/index.css';
 
+
+function List(){
+    return(
+        <TaskList/>
+    );
+}
+
+function Task(props){
+    return(
+        <li className='tasks-item'>{props.taskName}</li>
+    );
+}
+
+function TaskList(){
+    const taskListItem = ["Sky full of stars","Feed Fish","Clean workstation"];
+    return(
+        <ul>
+            {taskListItem.map((task, index) => {
+                return <Task key={index} taskName = {task} />;
+            })}
+        </ul>
+    )
+}
+
+function App() {
+    return(
+        <div className="app">
+            <header className="app-header">
+                <h1>My Amazing Todo-List App </h1>
+                <p> The most simple and amazing todo-list app</p>
+                <List/>
+            </header>
+        </div>
+    );
+}
+
+
 ReactDOM.render(
-  <App/>,
-  document.getElementById('root')
+<App/>,
+document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
